dbb37346d4b902f322628eecfdad8e1cda4bbdca
root <root@ip-172-31-94-119.ec2.internal>
2020-05-18 12:40:00 +0000

    first commit

--- flask-web-server/app.py
+++ flask-web-server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "Hello from the Flask web server"
+  return "Hello from the Flask sidhant new server"
 
 if __name__ == '__main__':
   app.run(port=4010, debug=True)
